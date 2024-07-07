839db1cb67361338dfdd0e215957da8ad7a92aac
WindWaker991 <seba.avergara@gmail.com>
2024-07-07 16:01:14 -0400

    feat: add AppController to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from './config/config.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [AuthModule, UserModule, DatabaseModule, ConfigModule],
+  controllers: [AppController],
 })
 export class AppModule {}
