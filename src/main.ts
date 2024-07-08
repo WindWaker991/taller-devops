79677da796e8eda0757ee89e54f405e04da29f6d
Maria Luisa <82680192+Maria-riv@users.noreply.github.com>
2024-07-07 22:03:53 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AllExceptionsFilter } from './common/filters/exception.filter';
 import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
